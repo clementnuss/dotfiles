e50e3cbe6576548a054dae0756dc308a948f225a
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2021-12-13 14:58:32 +0100

    improve vim-tmux clipboard config

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
+lvim.colorscheme = "dracula"
 lvim.transparent_window = true
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
@@ -193,6 +193,7 @@ lvim.plugins = {
     },
     {"dracula/vim"},
   {"christoomey/vim-tmux-navigator"},
+  {"vim-tmux-clipboard"},
 }
 
 vim.opt.timeoutlen = 300
