18f48ecdcc934e7a6b376a9198100b5a07491aaa
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2022-03-23 16:29:34 +0100

    wezterm: add config

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font("FiraCode NF"),
+  line_height=1.0,
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = "Builtin Solarized Light",
+}
