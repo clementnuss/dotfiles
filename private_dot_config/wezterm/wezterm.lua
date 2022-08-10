61c1aaf54490e0a90b835dfe9ceb7765a63c98aa
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2022-08-10 11:39:48 +0200

    chore(wezterm): switch font and theme

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,7 +1,10 @@
 local wezterm = require 'wezterm';
 return {
-  font = wezterm.font_with_fallback({ "Fira Code", }),
+  font = wezterm.font_with_fallback({ "Comic Code Ligatures", }),
   line_height=1.1,
   hide_tab_bar_if_only_one_tab = true,
-  color_scheme = "Tomorrow",
+  -- color_scheme = "seoulbones_light",
+  color_scheme = "zenwritten_light",
+  -- color_scheme = "Night Owlish Light",
+  -- color_scheme = "Github_custom",
 }
