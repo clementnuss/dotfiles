4e6f6df3b4d2e2238174d063a2854f806c26bb04
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2022-12-28 14:31:14 +0100

    chore: switch font switch to iTerm

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
 local wezterm = require 'wezterm';
 return {
   font = wezterm.font_with_fallback{
-    { family = "Comic Code Ligatures"},
+    { family = "Iosevka Term"},
     "Symbols NF",
   },
-  font_size=14,
+  font_size=16,
   line_height=1.2,
   hide_tab_bar_if_only_one_tab = true,
-  color_scheme = "Builtin Solarized Light",
+  -- color_scheme = "Builtin Solarized Light",
   -- color_scheme = "zenbones_light",
-  -- color_scheme = "BlulocoLight",
+  color_scheme = "BlulocoLight",
   -- color_scheme = "Night Owlish Light",
   -- color_scheme = "Github_custom",
   audible_bell = "Disabled",
