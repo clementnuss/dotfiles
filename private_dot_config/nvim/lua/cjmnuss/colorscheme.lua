2a2bd16221a45e4c10950046ce4cfc849677a8bd
Clément Nussbaumer <clement@astutus.org>
2023-04-13 08:57:16 +0200

    chore(nvim): simply config
    Signed-off-by: Clément Nussbaumer <clement@astutus.org>

--- private_dot_config/nvim/lua/cjmnuss/colorscheme.lua
+++ private_dot_config/nvim/lua/cjmnuss/colorscheme.lua
@@ -11,30 +11,7 @@
  -- 	return
  -- end
 
-require("github-theme").setup({
-  theme_style = "light",
-  -- function_style = "italic",
-  -- sidebars = {"qf", "vista_kind", "terminal", "packer"},
-
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  -- colors = {hint = "orange", error = "#ff0000"},
-
---   -- Overwrite the highlight groups
---   overrides = function(c)
---     return {
---       htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
---       DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
---       -- this will remove the highlight groups
---       TSField = {},
---     }
---   end
-})
-
--- require('solarized').set()
+vim.cmd('colorscheme github_light')
 
 require('lualine').setup()
 
--- https://github.com/navarasu/onedark.nvim
--- require('onedark').setup {
---     style = 'light'
--- }
