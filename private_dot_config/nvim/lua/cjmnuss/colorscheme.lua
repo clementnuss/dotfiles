7f18d94d8e7eabf987db57065f7730dcc5b10763
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2023-09-25 16:24:18 +0200

    chore(tmux-colors): fix 24bit colors issue
    Signed-off-by: Clément Nussbaumer <clement.nussbaumer@postfinance.ch>

--- private_dot_config/nvim/lua/cjmnuss/colorscheme.lua
+++ private_dot_config/nvim/lua/cjmnuss/colorscheme.lua
@@ -6,3 +6,12 @@ vim.cmd.colorscheme "catppuccin-latte"
 
 require('lualine').setup()
 
+require("catppuccin").setup({
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        leap = true,
+    }
+})
