bb67e2b9b79e39d1113a1d628a9d32e23f86ab0d
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2025-01-16 14:34:10 +0100

    feat(nvim): update config
    Signed-off-by: Clément Nussbaumer <clement.nussbaumer@postfinance.ch>

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,2 +1,6 @@
 -- https://github.com/folke/snacks.nvim/blob/main/docs/animate.md
 vim.g.snacks_animate = false
+
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+vim.g.autoformat = false
+
