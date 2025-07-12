e0b4d9b67e444e7f02c7fc0a69abf652366ea668
Clément Nussbaumer <clement@n8r.ch>
2025-07-12 07:17:25 +0200

    fix(wezterm): permit composed key with left/right alt!
    source https://microblog.desipenguin.com/post/accent-marks-wezterm/
    thanks @mandarvaze !

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -15,6 +15,9 @@ config.keys = {
   }
 }
 
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
 -- config.font_size=14
 -- config.line_height=1
 config.hide_tab_bar_if_only_one_tab = true
