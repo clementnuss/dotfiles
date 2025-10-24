48861093656d0481edaf63181753baf5b167fdaf
Clément Nussbaumer <clement@n8r.ch>
2025-10-24 17:43:47 +0200

    feat: save/restore lid brightness

--- private_dot_config/hypr/scripts/executable_lid-handler.sh
+++ private_dot_config/hypr/scripts/executable_lid-handler.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if grep -q closed /proc/acpi/button/lid/LID0/state; then
+    brightnessctl -s
     hyprctl keyword monitor "eDP-1, disable"
 else
     hyprctl keyword monitor "eDP-1, preferred, auto, 2"
+    brightnessctl -r
 fi
